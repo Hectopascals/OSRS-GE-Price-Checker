46ac7e532243fa039b0cc629916a5617fc356ece
Henry <henryy_@live.com>
2016-11-11 15:02:23 -0500

    Reads from a file now and outputs data

--- OSRSItemGetter.java
+++ OSRSItemGetter.java
@@ -1,6 +1,7 @@
 package osrsItemGet;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,30 +17,41 @@ public class OSRSItemGetter {
 		InputStream is = null;
 		InputStreamReader isr = null;
 		BufferedReader br = null;
+		BufferedReader brtxt = null;
 		int itemID;
 		String[] splitLine = null;
 		
-		System.out.println("Enter item num:");
-		Scanner scanner = new Scanner(System.in);
-		itemID = scanner.nextInt();
-		scanner.close();
-
+//		System.out.println("Enter item num:");
+//		Scanner scanner = new Scanner(System.in);
+//		itemID = scanner.nextInt();
+//		scanner.close();
+		
 		try {
-			url = new URL("https://api.rsbuddy.com/grandExchange?a=guidePrice&i="
-					+ itemID);
-			// Get inputstream thru URL Connection
-			con = url.openConnection();
-			is = con.getInputStream();
-			isr = new InputStreamReader(is);
-			br = new BufferedReader(isr);
+			brtxt = new BufferedReader(new FileReader("lazyInputItems.txt"));
+			String nextLine;
+			while ((nextLine = brtxt.readLine()) != null) {
+				itemID = Integer.parseInt(nextLine);
+				// read in even numbers, since id then item name
+				nextLine = brtxt.readLine();
+				System.out.println(nextLine);
+				url = new URL("https://api.rsbuddy.com/grandExchange?a=guidePrice&i="
+						+ itemID);
+				// Get inputstream thru URL Connection
+				con = url.openConnection();
+				is = con.getInputStream();
+				isr = new InputStreamReader(is);
+				br = new BufferedReader(isr);
 
-			String line = br.readLine();
-			if (line != null)
-				splitLine = line.split(",");
-			for (int i = 0; i < splitLine.length; i++) {
-				System.out.println(splitLine[i]);
-				// Get rid of everything except the numbers
-				//System.out.println(splitLine[i].replaceAll("\\D",  ""));
+				String line = br.readLine();
+				if (line != null)
+					splitLine = line.split(",");
+				
+				for (int i = 0; i < splitLine.length; i++) {
+					System.out.println(splitLine[i]);
+					// Get rid of everything except the numbers
+					//System.out.println(splitLine[i].replaceAll("\\D",  ""));
+				}
+				System.out.println();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
